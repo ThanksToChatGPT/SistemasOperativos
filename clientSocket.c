40745fa1c6eac2fd5b1579116f55be786c04a145
Juan DC <jucastanedaca@unal.edu.co>
2025-10-26 14:57:23 -0500

    Add files via upload

---
+++ clientSocket.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+
+
+int main() {
+    int fd, r, opcion;
+    bool exitProgram = false;
+    struct sockaddr_in cliente;
+    char artist[100], song[100], message[250], buffer[1024];
+    #define PORT 3535
+    // Para TCP se debe usar (SOCK_STREAM).
+    // Crear socket UDP
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+        perror("error al crear socket");
+        exit(-1);
+    }
+
+    // Configurar dirección del servidor al que se conecta
+    cliente.sin_family = AF_INET;
+    cliente.sin_port = htons(PORT);
+    cliente.sin_addr.s_addr = inet_addr("127.0.0.1");
+    bzero(&cliente.sin_zero, 8);
+
+    // Conectarse al servidor
+    r = connect(fd, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in));
+    if (r == -1) {
+        perror("error en connect");
+        close(fd);
+        exit(-1);
+    }
+     
+    while(!exitProgram  ){
+        printf("Digite el número de la acción que desea realizar:\n1. Buscar canción\n2. Buscar canciones de un artista\n3. Añadir una nueva canción de un artista\n4. Salir\n\n");
+        scanf("%d", &opcion);
+        printf("\n");
+
+        switch (opcion) {
+            case 1:
+                printf("\nDigite el nombre del Artista: ");
+                scanf("%[^\n]", artist);
+
+                printf("Digite el nombre de la Canción: ");
+                scanf("%[^\n]", song);
+
+                snprintf(message, sizeof(message), "1,%s,%s", artist, song);
+                r = send(fd, message, strlen(message), 0);
+                if (r == -1) {
+                    perror("error en send");
+                    exitProgram=true;
+                }
+                printf("\nMensaje enviado al servidor.\n");
+
+                r = recv(fd, buffer, sizeof(buffer) - 1, 0);
+                if (r == -1) {
+                    perror("error en recv");
+                    exitProgram=true;
+                } else if (r == 0) {
+                    printf("El servidor cerró la conexión.\n");
+                    exitProgram=true;
+                }
+
+                buffer[r] = '\0';
+                printf("\nRespuesta del servidor:\n%s\n", buffer);
+                break;
+
+            case 2:   
+                printf("\nDigite el nombre del Artista: ");
+                scanf(" %[^\n]", artist);
+
+                snprintf(message, sizeof(message), "2,%s", artist);
+
+                r = send(fd, message, strlen(message), 0);
+                if (r == -1) {
+                    perror("error en send");
+                    exitProgram=true;
+                }
+                printf("\nMensaje enviado al servidor.\n");
+
+                r = recv(fd, buffer, sizeof(buffer) - 1, 0); 
+                if (r == -1) {
+                    perror("error en recv");
+                    exitProgram=true;
+                } else if (r == 0) {
+                    printf("El servidor cerró la conexión.\n");
+                    exitProgram=true;
+                }
+
+                buffer[r] = '\0'; 
+                printf("\nRespuesta del servidor:\n%s\n", buffer);
+                break;
+
+            case 3:
+                printf("\nDigite el nombre del Artista que desea agregar: ");
+                scanf("%[^\n]", artist);
+
+                printf("Digite el nombre de la Canción que desea agregar: ");
+                scanf("%[^\n]", song);
+
+                snprintf(message, sizeof(message), "3,%s,%s", artist, song);
+                r = send(fd, message, strlen(message), 0);
+                if (r == -1) {
+                    perror("error en send");
+                    exitProgram=true;
+                }
+                printf("\nMensaje enviado al servidor.\n");
+
+                r = recv(fd, buffer, sizeof(buffer) - 1, 0);
+                if (r == -1) {
+                    perror("error en recv");
+                    exitProgram=true;
+                } else if (r == 0) {
+                    printf("El servidor cerró la conexión.\n");
+                    exitProgram=true;
+                }
+
+                buffer[r] = '\0';
+                printf("\nRespuesta del servidor:\n%s\n", buffer);
+                break;
+
+            case 4:
+                printf("\nSaliendo del programa...\n");
+                exitProgram=true;
+                break;
+
+            default:
+                printf("\nOpción no válida.\n");
+                break;
+        }
+    }
+
+    close(fd);
+    printf("\nPrograma finalizado.\n");
+    return 0;
+}
